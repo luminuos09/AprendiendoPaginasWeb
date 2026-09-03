e11700770ffddebd14bee173748ff1fd4767453b
luminuos09 <sr053990@gmail.com>
2026-09-03 12:35:53 -0500

    aprendiendo funciones + tarqea

---
+++ aprendiendoJS/Funciones.js
@@ -0,0 +1,22 @@
+function showMessage(){
+    console.log("Mensaje")
+    
+}
+showMessage()
+
+//funciones de callback
+
+//funciones flechas
+  //pasar a funtiones flecha esta funcion ask
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+
