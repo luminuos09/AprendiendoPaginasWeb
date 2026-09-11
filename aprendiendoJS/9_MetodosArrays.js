2483f187233a385bd9cf6bdc3d79360ccc26d4fc
luminuos09 <sr053990@gmail.com>
2026-09-10 20:52:48 -0500

    aprendiendo un poco de los metodos en los arrays

--- aprendiendoJS/9_MetodosArrays.js
+++ aprendiendoJS/9_MetodosArrays.js
@@ -3,7 +3,7 @@ compu.push("voy")
 compu.push("a")
 compu.push("casa")
 //delete en estos casos no sirve pq solo elimina la variable pero no el dato en la memoria
-//entonces para esos casos es mejor el slice
+//entonces para esos casos es mejor el splice
 compu.splice(1,1)//desde el indice 1, remover 1 elemento
 
 console.log(compu)
