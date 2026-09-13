90e8b3d6571c3833c93f80c7e177b410e95d68e3
luminuos09 <sr053990@gmail.com>
2026-09-12 21:23:43 -0500

    terminando de aprender los metodos en los arrays

--- aprendiendoJS/9_MetodosArrays.js
+++ aprendiendoJS/9_MetodosArrays.js
@@ -7,3 +7,47 @@ compu.push("casa")
 compu.splice(1,1)//desde el indice 1, remover 1 elemento
 
 console.log(compu)
+//Buscar dentro de una array
+if(compu.includes('voy')){
+    console.log("Existe")
+}else{
+    console.log("No existe")
+}
+
+//buscar objetos dentro de un array
+let users = [
+  {id: 1, name: "Celina"},
+  {id: 2, name: "David"},
+  {id: 3, name: "Federico"}
+];
+
+let user = users.find(item => item.id == 1);
+
+console.log(user.name); // Celina
+
+
+//ordenar una array
+let numeros = [3, 1, 10, 2];
+
+numeros.sort();                        //  ¡Mal! Ordena como texto
+console.log(numeros); // [1, 10, 2, 3]
+
+numeros.sort((a, b) => a - b);         //  Ordena bien
+console.log(numeros); // [1, 2, 3, 10]
+
+//buscar dentro de una array
+let frutas = ["manzana", "banano", "uva"];
+
+frutas.forEach(function(fruta, indice) {
+  console.log(indice + ": " + fruta);
+});
+
+//reduce ayuda a acumular todos los elementos de una array  
+let precios = [100, 200, 300];
+
+let total = precios.reduce(function(acumulador, precio) {
+  return acumulador + precio;
+}, 0);  // <- el 0 es el valor inicial
+
+console.log(total); // 600
+
